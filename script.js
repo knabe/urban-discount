330a508be9878a1c2d5f37813152dd112a91de83
CrystallineDex <alexxr87@gmail.com>
2015-05-24 16:25:27 -0400

    Awesome update.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- var stockLevelExtension = function(){
+var stockLevelExtension = function(){
     Array.prototype.contains = function(obj) {
         var i = this.length;
         while (i--) {
@@ -14,14 +14,15 @@
         partialDiscount : ['BEAUTY','APARTMENT','MUSIC'] //these things are only 20% off
     }
 
-    document.addEventListener('click', function() {
-        init(angular.element('[ng-controller=MainCtrl]').scope().serviceGroup.products.productList[0].prodData.prices.high)
-    }, false)
+    // document.addEventListener('click', function() {
+       // init(angular.element('[ng-controller=MainCtrl]').scope().serviceGroup.products.productList[0].prodData.prices.high)
+    // }, false)
 
+    // Product Detail Page Init
     var init = function(price){
 
         if(price !== undefined) {
-            var discount = isPartialDiscount ? .2 : .4,
+            var discount = isPartialDiscount() ? .2 : .4,
             qtyContainer = document.getElementById("urban-discount-plugin");
 
             price = price - (price * discount);
@@ -39,11 +40,36 @@
             else{
                 document.getElementsByClassName('urban-discount-extension-price')[0].innerHTML = formatCurrency(price);
             }
+        }
+    }
 
-            // If we're on a product loop page
-            document.getElementsByClassName('product').every(function(element, index, array){
-                console.log(index);
-            });
+    // Category Page Init
+    var categoryInit = function(){
+        var discount = isPartialDiscount() ? .2 : .4;
+        // If we're on a product loop page
+        var productArray = document.getElementsByClassName('product');
+        for(var i=0, arrayLen = productArray.length; i < arrayLen; i++){
+
+            var priceElement;
+
+            if(productArray[i].getElementsByClassName('price-old')[0]){
+                priceElement = productArray[i].getElementsByClassName('price-old')[0];
+            }
+            else if(productArray[i].getElementsByClassName('price-promo')[0]){
+                priceElement = productArray[i].getElementsByClassName('price')[0];
+                //tempPrice.toString();
+            }
+            else{
+                priceElement = productArray[i].getElementsByClassName('price')[0];
+            }
+
+            // priceElement = priceElement.innerHTML.substring(0,priceElement.innerHTML.indexOf('<'));
+            var price  = parseFloat(priceElement.innerHTML.replace(/[^\d\.]/g,''));
+
+            price = formatCurrency( price - (price * discount) );
+            console.log(price);
+
+            productArray[i].getElementsByClassName('price')[0].insertAdjacentHTML('beforeend', ' - <span class="urban-discount-extension-price">' + price + '</span>');
         }
     }
 
@@ -58,29 +84,35 @@
     }
 
     // Format Price
-    var formatCurrency = function(price){
-        price.toFixed(2);
-        price = '$' + price;
-        return price;
+    var formatCurrency = function(formatPrice){
+        formatPrice = formatPrice.toFixed(2);
+        formatPrice = '$' + formatPrice;
+        return formatPrice;
     }
 
 
     function stockLoop() {
-        //loop through angular scope no more than 6 times 
-        if(loopcounter <= 6){
-            loopcounter++;
-            setTimeout(function () {
-                if(loopcounter <= 6){
-                    var prodList = angular.element('[ng-controller=MainCtrl]').scope().serviceGroup.products.productList[0].prodData.prices.high;
-                    if(prodList !== undefined){
-                        init(prodList);
-                        //kill the loop
-                        loopcounter = 9;
+        if(document.URL.indexOf("productdetail.jsp") > -1){
+            //loop through angular scope no more than 6 times
+            if(loopcounter <= 6){
+                loopcounter++;
+                setTimeout(function () {
+                    if(loopcounter <= 6){
+                        var prodList = angular.element('[ng-controller=MainCtrl]').scope().serviceGroup.products.productList[0].prodData.prices.high;
+                        if(prodList !== undefined){
+                            init(prodList);
+                            //kill the loop
+                            loopcounter = 9;
+                        }
                     }
-                }
 
-                stockLoop();
-            }, 1000);
+                    stockLoop();
+                }, 1000);
+            }
+        }
+
+        if(document.URL.indexOf("category.jsp") > -1){
+            categoryInit();
         }
     }
 
