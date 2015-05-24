bd7bd6fa5c83c4212c7a9a7e534e4a74182dda3a
Tom Knabe <TKnabe@urbanoutfitters.com>
2015-05-24 14:26:37 -0400

    dammit alex

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-
-
  var stockLevelExtension = function(){
+    Array.prototype.contains = function(obj) {
+        var i = this.length;
+        while (i--) {
+            if (this[i] === obj) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-     var loopcounter = 0;
+    var loopcounter = 0,
+    config = {
+        partialDiscount : ['BEAUTY','APARTMENT','MUSIC'] //these things are only 20% off
+    }
 
     document.addEventListener('click', function() {
         init(angular.element('[ng-controller=MainCtrl]').scope().serviceGroup.products.productList[0].prodData.prices.high)
     }, false)
 
-	var init = function(price){
+    var init = function(price){
+        
         if(price !== undefined) {
-            price = (price - (price * .4) );
-            var priceContainer = document.getElementById("urban-discount-plugin");
-            if(priceContainer != null){
-                priceContainer.parentNode.removeChild(priceContainer);
+            var discount = isPartialDiscount ? .2 : .4,
+            qtyContainer = document.getElementById("urban-discount-plugin");
+
+            price = '$' + (price - (price * discount) ).toFixed(2)
+
+            if(qtyContainer != null){
+                qtyContainer.parentNode.removeChild(qtyContainer);
             }
             document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeend', '<h2 id="urban-discount-plugin">'+price+'</h2>');
-
-            if(!document.getElementsByClassName('urban-discount-extension-price')[0]){
-                document.getElementsByClassName('product-price')[0].insertAdjacentHTML('beforeend', '<span class="urban-discount-extension-price">' + formatCurrency(parseFloat(price)) + '</span>');
-            }
-
-            else{
-                document.getElementsByClassName('urban-discount-extension-price')[0].innerHTML = formatCurrency(parseFloat(price));
-            }
-
-            // If we're on a product loop page
-            document.getElementsByClassName('product').every(function(element, index, array){
-                console.log(index);
-            });
-
         }
+    }
+
+    var stockParentCategory = function(){
+        var elem = document.getElementsByClassName("header-nav-data")
+        return elem[0].getAttribute('data-selected');
+    }
+
+    var isPartialDiscount = function(){
+        var parentCat = stockParentCategory()
+        return (config.partialDiscount.contains(parentCat) ? true : false);
     }
 
     function stockLoop() {
@@ -56,10 +66,3 @@
 }
 
 stockLevelExtension();
-
-// Format Price
-function formatCurrency(price){
-    price.toFixed(2);
-    price = '$' + price;
-    return price;
-}
